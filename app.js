a85e89c4a5741d9e0b391293cd059a9766738009
Fernando Castro <castrofernandomario@gmail.com>
2023-04-12 21:52:37 -0300

    prompt_1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-function comprarPantalones(cantidad, precio) {
+function comprarPantalones(precio) {
+    let cantidad = prompt("¿Cuántos pantalones desea comprar?");
     let total = cantidad * precio;
-    
+  
     if (cantidad > 0 && precio > 0) {
       if (cantidad >= 3) {
         total *= 0.8; // Aplicamos un descuento del 20% si se compran 3 o más pantalones
@@ -9,14 +10,12 @@ function comprarPantalones(cantidad, precio) {
       for (let i = 1; i <= cantidad; i++) {
         console.log(`Pantalón ${i} comprado por ${precio} pesos`);
       }
-      
+  
       console.log(`El total de la compra es ${total} pesos.`);
     } else {
       console.log("La cantidad y el precio deben ser números positivos.");
     }
   }
-//ejemplo 1//
-  comprarPantalones(3, 2500);
-
-//ejemplo 2 //
-  comprarPantalones(2, 2500);
+  
+  //ejemplo
+  comprarPantalones(2500);
